907c1858250015b7b37e0fdb2d841d0d3b8884dd
bcg-ygpark <ygpark@blockchainglobal.co.kr>
2025-07-15 17:03:09 +0900

    refactor(packages/ui): Naive UI 기반 컴포넌트 전체 삭제 및 Headless UI + Tailwind CSS 전략 전환 준비

--- packages/ui/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-// UI 컴포넌트들을 export
-export { default as BaseButton } from './BaseButton.vue';
-export { default as BaseForm } from './BaseForm.vue';
-export { default as BaseInput } from './BaseInput.vue';

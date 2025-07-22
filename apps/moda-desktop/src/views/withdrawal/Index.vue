988897d17c181dd144c2dcb3cf3af4736b4376a8
pcjin <pcjin@blockchainglobal.co.kr>
2025-07-22 15:02:27 +0900

    fix: 화면 적용 변경

--- apps/moda-desktop/src/views/withdrawal/Index.vue
+++ apps/moda-desktop/src/views/withdrawal/Index.vue
@@ -1,2 +1,100 @@
-<template>출금신청 화면입니다</template>
-<script setup lang="ts"></script>
+<template>
+  <div class="flex flex-col gap-6">
+    <MainCardContent title="출금신청">
+      <template #content>
+        <div class="flex gap-3 items-center">
+          <SubCardContent title="계좌번호">
+            <template #content> <> </template>
+          </SubCardContent>
+          <p>></p>
+          <SubCardContent title="받을 계좌번호">
+            <template #content>
+              <div class="flex gap-2 items-center">
+                <BaseInputText
+                  v-model="accountNumber"
+                  :showIcon="false"
+                  placeholder="계좌번호를 입력하세요"
+                  class="w-[240px]"
+                />
+              </div>
+            </template>
+          </SubCardContent>
+          <p>></p>
+          <SubCardContent title="신청금액">
+            <template #content>
+              <div class="flex gap-2 items-center">
+                <BaseInputText class="w-[140px]" type="number" v-model="amount" :showIcon="false" />
+                <BaseButton variant="outline" label="전액" class="whitespace-nowrap" />
+              </div>
+            </template>
+          </SubCardContent>
+          <p>></p>
+          <BaseButton class="px-[43px]" variant="primary" label="신청하기" />
+        </div>
+      </template>
+    </MainCardContent>
+
+    <MainCardContent title="신청내역">
+      <template #content>
+        <BaseTable
+          :headers="headers"
+          :data="data"
+          :selectable="selectable"
+          :sortable="sortable"
+          @sort="handleSort"
+        />
+      </template>
+    </MainCardContent>
+  </div>
+</template>
+<script setup lang="ts">
+import { BaseButton, BaseTable, BaseInputText } from '@template/ui';
+import MainCardContent from '@/components/ui/cards/MainCardContent.vue';
+import SubCardContent from '@/components/ui/cards/SubCardContent.vue';
+import type { TableHeader, TableRow } from '@template/ui';
+import { ref } from 'vue';
+
+const amount = ref('0');
+const accountNumber = ref('');
+
+const headers: TableHeader[] = [
+  { key: 'date', title: '신청일자', width: '140px' },
+  { key: 'account', title: '계좌번호', width: '200px', align: 'center' },
+  { key: 'type', title: '구분', width: '422px', align: 'center' },
+  { key: 'amount', title: '금액($)', width: '422px', align: 'right' },
+  { key: 'apply', title: '신청일시', width: '220px', align: 'center' },
+  { key: 'complete', title: '처리일시', width: '220px' },
+  { key: 'note', title: '비고', width: '200px' },
+];
+
+const data: TableRow[] = [
+  {
+    id: 1,
+    date: '2025-05-22',
+    account: '002-01-000001',
+    type: '출금',
+    amount: '20,000',
+    apply: '2025-05-22 14:02:10',
+    complete: '2025-05-22 14:02:15',
+    note: '-',
+  },
+  {
+    id: 2,
+    date: '2025-05-22',
+    account: '002-01-000001',
+    type: '이체',
+    amount: '100',
+    apply: '2025-05-13 13:30:57',
+    complete: '2025-05-15 13:30:57',
+    note: '-',
+  },
+];
+
+const selectable = true;
+const sortable = true;
+
+const handleSort = (key: string, direction: 'asc' | 'desc') => {
+  console.log(`Sorting by ${key} in ${direction} direction`);
+};
+</script>
+<style scoped></style>

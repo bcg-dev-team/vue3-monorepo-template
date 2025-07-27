cb68c6a1d26c4faaada54a7e35a04301099b2008
pcjin <pcjin@blockchainglobal.co.kr>
2025-07-28 08:12:03 +0900

    feat: mypage 컴포넌트 분리 및 ui 디렉토리 재할당
    - components/ui -> components/common
      - ui라는 명칭이 직관적이지 않음 - ui 패키지와 역할 분리, common은
        프로젝트에서 사용하는 공통 컴포넌트
    - mypage관련 컴포넌트 분리
      - UserInfo.vue
      - UserAssetsInfo.vue
    - views의 디렉토리와 1:1 대응 되도로 components 디렉토리 생성
      - .gitkeep 임시파일 생성 - 작업시 지우고 사용

---
+++ apps/moda-desktop/src/components/mypage/UserInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bg-primary px-6 pt-9 pb-6">
+    <div class="flex items-center gap-2">
+      <span class="text-2xl font-bold">{{ userInfo.name }}</span>
+    </div>
+    <div class="mt-2">
+      <span class="text-sm">마지막 로그인: {{ userInfo.lastLogin }}</span>
+    </div>
+  </div>
+  <div class="p-6">
+    <div class="flex flex-col gap-3">
+      <div class="py-3 px-4 bg-bg-innerframe rounded-md">
+        <span class="text-primary">:편지: </span>
+        <span class="text-md font-medium">{{ userInfo.email }}</span>
+      </div>
+      <div class="py-3 px-4 bg-bg-innerframe rounded-md">
+        <span class="text-primary">:전화: </span>
+        <span class="text-md font-medium">{{ userInfo.phone }}</span>
+      </div>
+    </div>
+    <div class="w-[282px] h-[48px] bg-primary rounded-md flex items-center justify-center mt-4">
+      <span class="text-sm font-semibold">버튼 영역</span>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const userInfo = reactive({
+  name: '김트레이더',
+  lastLogin: '2025.07.22 14:30',
+  email: 'traderkim@email.com',
+  phone: '010-1234-5678',
+});
+</script>

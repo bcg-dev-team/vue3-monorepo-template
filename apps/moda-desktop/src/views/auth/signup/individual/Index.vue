11e7789c227796743e357b4bf4c2c193118b6ee6
pcjin <pcjin@blockchainglobal.co.kr>
2025-07-29 11:34:46 +0900

    feat: 로그인 화면 일부 레이아웃 구성

--- apps/moda-desktop/src/views/auth/signup/individual/Index.vue
+++ apps/moda-desktop/src/views/auth/signup/individual/Index.vue
@@ -2,7 +2,9 @@
   <div class="flex items-center justify-center min-h-screen">
     <MainCardContent>
       <template #content>
-        <IndividualTerms />
+        <IndividualTerms v-if="step === 1" />
+        <IndividualPhone v-if="step === 2" />
+        <IndividualInfo v-if="step === 3" />
       </template>
     </MainCardContent>
   </div>
@@ -10,4 +12,9 @@
 <script lang="ts" setup>
 import MainCardContent from '@/components/common/cards/MainCardContent.vue';
 import IndividualTerms from '@/components/auth/signup/individual/IndividualTerms.vue';
+import IndividualPhone from '@/components/auth/signup/individual/IndividualPhone.vue';
+import IndividualInfo from '@/components/auth/signup/individual/IndividualInfo.vue';
+import { ref } from 'vue';
+
+const step = ref(3);
 </script>

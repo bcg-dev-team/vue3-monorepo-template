ee271adc90bd12a4d1895a61b4cb28f423d54035
pcjin <pcjin@blockchainglobal.co.kr>
2025-07-31 08:36:09 +0900

    chore: sample-desktop 생성

--- apps/sample-desktop/src/views/auth/login/Index.vue
+++ apps/sample-desktop/src/views/auth/login/Index.vue
@@ -1,10 +1,18 @@
 <template>
+<<<<<<< HEAD
   <div class="flex min-h-screen items-center justify-center">
+=======
+  <div class="flex items-center justify-center min-h-screen">
+>>>>>>> 45118e1 (chore: sample-desktop 생성)
     <MainCardContent>
       <template #content> </template>
     </MainCardContent>
   </div>
 </template>
+<<<<<<< HEAD
 <script lang="ts" setup>
 import MainCardContent from '@/components/common/cards/MainCardContent.vue';
 </script>
+=======
+<script lang="ts" setup></script>
+>>>>>>> 45118e1 (chore: sample-desktop 생성)

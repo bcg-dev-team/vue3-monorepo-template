cdbc067efed99477bbc3c65a80e9aa566c311ab5
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-05 14:12:04 +0900

    feat: 비밀번호 재설정 - 이메일 입력 퍼블리싱

---
+++ apps/sample-desktop/src/views/auth/resetPassword/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex min-h-screen items-center justify-center">
+    <MainCardContent>
+      <template #content>
+        <ResetPasswordEmailForm v-if="step === 0" />
+      </template>
+    </MainCardContent>
+  </div>
+</template>
+<script lang="ts" setup>
+import ResetPasswordEmailForm from '@/components/auth/resetPassword/ResetPasswordEmailForm.vue';
+import MainCardContent from '@/components/common/cards/MainCardContent.vue';
+import { ref } from 'vue';
+
+const step = ref(0);
+</script>

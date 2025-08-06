bd328bc48bd19525b7dd390d800bbe98690aa992
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-06 15:34:48 +0900

    feat: 로그인 화면 퍼블리싱

--- apps/sample-desktop/src/views/auth/login/Index.vue
+++ apps/sample-desktop/src/views/auth/login/Index.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="flex min-h-screen items-center justify-center">
     <MainCardContent>
-      <template #content> </template>
+      <template #content>
+        <BaseLogin />
+      </template>
     </MainCardContent>
   </div>
 </template>
 <script lang="ts" setup>
 import MainCardContent from '@/components/common/cards/MainCardContent.vue';
+import BaseLogin from '@/components/auth/login/BaseLogin.vue';
 </script>

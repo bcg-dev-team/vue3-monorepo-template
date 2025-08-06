19e3f0081ff80078cc4a4761e3a3564b89a441fb
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-06 09:26:05 +0900

    feat: 회원가입 진입 화면 퍼블리싱
    - 개인/법인 선택 가능
    - 개인 법인 선택 후 각 회원가입 화면 진입 가능

--- apps/sample-desktop/src/components/auth/signup/SignUpIntro.vue
+++ apps/sample-desktop/src/components/auth/signup/SignUpIntro.vue
@@ -1,10 +1,30 @@
 <template>
   <AuthContent title="회원가입" description="개인회원과 법인회원 중 선택해주세요.">
     <template #content>
-      <div class="mt-12">버튼 영역 개인|법인</div>
-      <div class="mt-[33px] w-[360px]">버튼 영역 다음</div>
-      <div class="flex justify-center mt-6">
-        <a class="underline text-[13px] font-regular cursor-pointer">이미 아이디가 있어요</a>
+      <div class="gap-size-12 flex items-center justify-center">
+        <BaseButton
+          class="h-[80px]"
+          :variant="type === 'individual' ? 'light-solid' : 'outline'"
+          label="개인회원"
+          @click="type = 'individual'"
+        />
+        <BaseButton
+          class="h-[80px]"
+          :variant="type === 'corporation' ? 'light-solid' : 'outline'"
+          label="법인회원"
+          @click="type = 'corporation'"
+        />
+      </div>
+      <div class="mt-[33px] w-[360px]">
+        <BaseButton
+          size="regular"
+          label="다음"
+          :variant="type ? 'primary' : 'disabled'"
+          @click="onClickNext"
+        />
+      </div>
+      <div class="mt-6 flex justify-center">
+        <a class="font-regular cursor-pointer text-[13px] underline">이미 아이디가 있어요</a>
       </div>
     </template>
   </AuthContent>
@@ -12,4 +32,18 @@
 
 <script lang="ts" setup>
 import AuthContent from '@/components/auth/AuthContent.vue';
+import { BaseButton } from '@template/ui';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+
+const router = useRouter();
+const type = ref<'individual' | 'corporation' | null>(null);
+
+const onClickNext = () => {
+  if (type.value === 'individual') {
+    router.push({ name: 'individual-sign-up' });
+  } else if (type.value === 'corporation') {
+    router.push({ name: 'corporate-sign-up' });
+  }
+};
 </script>

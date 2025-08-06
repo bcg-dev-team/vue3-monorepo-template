3b252a34ee1893ba1138c68325914558082b9ee2
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-06 10:59:38 +0900

    feat: 개인회원가입 - 약관동의 화면 퍼블리싱
    - Checkbox Label 공통 컴포넌트 생성

---
+++ apps/sample-desktop/src/components/auth/common/CheckBoxLabel.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="gap-size-4 flex items-center">
+    <BaseCheckbox v-model="isChecked" />
+    <slot> </slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { BaseCheckbox } from '@template/ui';
+
+const isChecked = defineModel<boolean>('isChecked', { required: true });
+</script>
--- apps/sample-desktop/src/components/auth/signup/individual/IndividualTerms.vue
+++ apps/sample-desktop/src/components/auth/signup/individual/IndividualTerms.vue
@@ -1,19 +1,73 @@
 <template>
   <AuthContent title="개인회원 가입" description="가입 약관을 확인해주세요.">
     <template #header>
-      <div class="mb-12">탑 버튼 영역</div>
+      <div class="flex items-center justify-between">
+        <BaseIcon name="arrow-backward" size="md" />
+        <BasePaginationJoin :count="3" :current="0" />
+      </div>
     </template>
     <template #content>
-      <div class="mt-12">
+      <div>
         <div class="border-b-2 border-black pb-[10px]">
-          <h2>가입 약관 동의</h2>
+          <h2 class="text-font-14 font-semibold">가입 약관 동의</h2>
         </div>
-        <div class="mt-6">체크박스 영역</div>
+        <div class="mt-6">
+          <div class="p-size-12 border-bg-outline rounded-[10px] border">
+            <CheckBoxLabel v-model:isChecked="isChecked">
+              <span class="text-font-15 font-semibold">모든 약관에 동의합니다.</span>
+            </CheckBoxLabel>
+          </div>
+          <div class="gap-size-8 px-size-12 mt-size-8 flex flex-col">
+            <CheckBoxLabel v-model:isChecked="isChecked">
+              <div class="text-font-12 flex w-full items-center justify-between">
+                <div class="font-medium">
+                  서비스 이용 약관 동의
+                  <span class="text-red">(필수)</span>
+                </div>
+                <div class="gap-size-2 flex items-center">
+                  상세보기
+                  <BaseIcon name="arrow-right-thin" size="sm" />
+                </div>
+              </div>
+            </CheckBoxLabel>
+            <CheckBoxLabel v-model:isChecked="isChecked">
+              <div class="text-font-12 flex w-full items-center justify-between">
+                <div class="font-medium">
+                  개인정보 수집 및 이용 동의
+                  <span class="text-red">(필수)</span>
+                </div>
+                <div class="gap-size-2 flex items-center">
+                  상세보기
+                  <BaseIcon name="arrow-right-thin" size="sm" />
+                </div>
+              </div>
+            </CheckBoxLabel>
+            <CheckBoxLabel v-model:isChecked="isChecked">
+              <div class="text-font-12 flex w-full items-center justify-between">
+                <div class="font-medium">
+                  마케팅 활용 및 광고성 정보 수신 동의
+                  <span class="text-default-muted">(선택)</span>
+                </div>
+                <div class="gap-size-2 flex items-center">
+                  상세보기
+                  <BaseIcon name="arrow-right-thin" size="sm" />
+                </div>
+              </div>
+            </CheckBoxLabel>
+          </div>
+        </div>
+        <div class="mt-[33px] w-[360px]">
+          <BaseButton size="regular" label="가입하기" variant="primary" />
+        </div>
       </div>
-      <div class="mt-[33px] w-[360px]">버튼 영역 다음</div>
     </template>
   </AuthContent>
 </template>
 <script lang="ts" setup>
+import { BaseIcon, BasePaginationJoin, BaseButton } from '@template/ui';
+import CheckBoxLabel from '@/components/auth/common/CheckBoxLabel.vue';
 import AuthContent from '@/components/auth/AuthContent.vue';
+import { ref } from 'vue';
+
+const isChecked = ref(false);
 </script>

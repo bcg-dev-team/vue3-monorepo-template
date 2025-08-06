541129089977dbe27a622273cee9ca6e07b2c9b4
bcg-ygpark <ygpark@blockchainglobal.co.kr>
2025-08-06 10:18:55 +0900

    feat(packages/ui): BaseChip, BaseCheckbox export 추가
    - BaseChip, BaseCheckbox export 추가

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -5,6 +5,16 @@ export { default as BaseInputText } from './BaseInput/BaseInputText.vue';
 export { default as BaseInputPassword } from './BaseInput/BaseInputPassword.vue';
 export { default as BaseProgressBar } from './BaseProgressBar/BaseProgressBar.vue';
 export { default as BasePasswordStrength } from './BasePasswordStrength/BasePasswordStrength.vue';
+export { default as BaseChip } from './BaseChips/BaseChip.vue';
+export { default as BaseCheckbox } from './BaseCheckbox/BaseCheckbox.vue';
+
+// TODO: BaseChip 컴포넌트 재설계 이후 제거 예정
+// export { default as ChipPosition } from './BaseChips/ChipPosition.vue';
+// export { default as ChipStatus } from './BaseChips/ChipStatus.vue';
+// export { default as ChipNotice } from './BaseChips/ChipNotice.vue';
+// export { default as ChipUpdown } from './BaseChips/ChipUpdown.vue';
+// export { default as ChipQna } from './BaseChips/ChipQna.vue';
+
 export * from './BasePagination';
 export * from './BaseTable';
 export * from './BaseIcon';

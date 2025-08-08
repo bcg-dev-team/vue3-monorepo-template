e369ce86108704c5c2ab7f6e44775f2070cd912e
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-08 10:07:25 +0900

    feat: 고객지원 - 공지사항 화면 퍼블리싱

--- apps/sample-desktop/src/components/service/Notice.vue
+++ apps/sample-desktop/src/components/service/Notice.vue
@@ -1,3 +1,30 @@
-<template name="content">안녕</template>
+<template>
+  <div class="gap-size-64 flex">
+    <div class="min-w-[500px]">
+      <div class="gap-size-4 flex items-center">
+        <BaseInput placeholder="여긴 '셀렉트 박스'가 들어갈 거에요" />
+        <BaseInput placeholder="검색어 입력 후 Enter" />
+      </div>
+    </div>
+    <div
+      class="bg-bg-bg-innerframe border-bg-bg-outline px-padding-48 py-padding-24 min-h-[794px] w-full rounded-[10px] border"
+    >
+      <div class="py-padding-16 gap-size-12 flex flex-col border-b">
+        <div class="gap-size-12 flex items-center">
+          <BaseChip label="업무공지" size="md" variant="grey" />
+          <span class="text-font-13 text-default-muted">2025.05.26</span>
+        </div>
+        <div>
+          <span class="text-font-18 font-medium"
+            >신용공여종목군 및 위탁증거금률 변경 안내(현대건설 외 3종목)</span
+          >
+        </div>
+      </div>
+      <div class="py-padding-36">내용 영역</div>
+    </div>
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { BaseChip, BaseInput } from '@template/ui';
+</script>

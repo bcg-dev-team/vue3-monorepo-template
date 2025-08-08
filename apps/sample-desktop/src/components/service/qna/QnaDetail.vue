b04214257c0d5c51d492b5c28c1fb4be5d541637
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-08 13:18:49 +0900

    feat: 고객지원 1:1 문의 화면 퍼블리싱
    - 1:1 문의 입력 및 상세 페이지 테이블 디자인 적용
      - /assets/acss/components/qna-table.scss
    - 1:1 문의 목록/상세/입력 화면 이동 설정
      - 우선 ref 로 적용 추후 상세 조정 필요

---
+++ apps/sample-desktop/src/components/service/qna/QnaDetail.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="gap-size-48 flex flex-col">
+    <div>
+      <div>
+        <span class="text-font-20 font-semibold">문의 내용</span>
+      </div>
+      <div class="mt-size-8">
+        <table class="qna-table">
+          <tr>
+            <th>제목</th>
+            <td>
+              <BaseChip class="mr-size-10" label="답변완료" variant="red" size="sm" />
+              출금신청이 잘 됐는지 문의드립니다!
+            </td>
+            <th>작성일시</th>
+            <td>2025.05.25 09:15:46</td>
+          </tr>
+          <tr>
+            <th>카테고리</th>
+            <td colspan="3">출금/예치</td>
+          </tr>
+          <tr>
+            <th>내용</th>
+            <td class="qna-content" colspan="3">
+              <div class="min-h-size-200">
+                출금신청이 잘 되었는지 모르겠어서 문의드립니다.<br />
+                신청 이름: 김주석<br />
+                신한은행 110-81-2301410
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th>첨부파일</th>
+            <td colspan="3">
+              <a class="text-blue underline" href="#">20250601_Screenshot_14.00.01.jpg</a>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <div>
+      <div>
+        <span class="text-font-20 font-semibold">관리자 답변</span>
+      </div>
+      <div class="mt-size-8">
+        <table class="qna-table">
+          <tr>
+            <th>제목</th>
+            <td>답변 드립니다.</td>
+            <th>답변일시</th>
+            <td>2025.05.25 09:15:46</td>
+          </tr>
+          <tr>
+            <th>카테고리</th>
+            <td colspan="3">출금/예치</td>
+          </tr>
+          <tr>
+            <th>내용</th>
+            <td class="qna-content" colspan="3">
+              <div class="min-h-size-200"></div>
+            </td>
+          </tr>
+          <tr>
+            <th>첨부파일</th>
+            <td colspan="3"></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class="mt-size-24 flex justify-center">
+    <BaseButton label="확인" size="large" @click="type = null" />
+  </div>
+</template>
+<script setup lang="ts">
+import { BaseButton, BaseChip } from '@template/ui';
+
+const type = defineModel<null | 'register' | 'detail'>('type', { required: true });
+</script>
--- apps/sample-desktop/src/views/service/Index.vue
+++ apps/sample-desktop/src/views/service/Index.vue
@@ -20,14 +20,15 @@
 import MainCardContent from '@/components/common/cards/MainCardContent.vue';
 import Notice from '@/components/service/notice/Notice.vue';
 import Event from '@/components/service/event/Event.vue';
+import QnA from '@/components/service/qna/QnaMain.vue';
 import { BaseTabs } from '@template/ui';
 import { ref } from 'vue';
 
-const modelValue = ref('notice');
+const modelValue = ref('qna');
 const tabs = [
   { value: 'notice', label: '공지사항', content: Notice },
   { value: 'event', label: '이벤트', content: Event },
-  { value: 'qna', label: '1:1 문의' },
+  { value: 'qna', label: '1:1 문의', content: QnA },
   { value: 'faq', label: '자주 묻는 질문' },
   { value: 'guide', label: '이용가이드' },
 ];

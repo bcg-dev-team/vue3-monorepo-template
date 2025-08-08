eb424cbcfc3178426863f46ce7574e497de66b21
bcg-hyunwoo <hyunwoo@blockchainglobal.co.kr>
2025-08-08 13:47:38 +0900

    refactor: 사용하지 않는 chipdata interface 속성 제거

--- packages/ui/src/types/components.ts
+++ packages/ui/src/types/components.ts
@@ -40,16 +40,9 @@ export interface TabItem {
 // BaseChip
 export type ChipVariant = 'grey' | 'red' | 'green' | 'blue' | 'yellow' | 'purple';
 
-// BaseChip 접근성 관련 타입
-export type ChipRole = 'button';
-
 // BaseChip 확장 타입
 export interface ChipData {
   label: string;
   variant: ChipVariant;
-  draggable?: boolean;
   icon?: string;
-  avatar?: string;
-  role?: ChipRole;
-  tabindex?: number;
 }

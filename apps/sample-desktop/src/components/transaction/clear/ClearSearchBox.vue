cc09ffb74470473a2ae7ab40dc6d8fa733cdc7cb
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-12 11:29:01 +0900

    feat: 거래내역 - 청산손익 화면 퍼블리싱

--- apps/sample-desktop/src/components/transaction/clear/ClearSearchBox.vue
+++ apps/sample-desktop/src/components/transaction/clear/ClearSearchBox.vue
@@ -4,9 +4,7 @@
       <LabelContent label="계좌번호" size="md">
         <BaseInput size="sm" placeholder="input select 들어올 예정" />
       </LabelContent>
-      <LabelContent label="주문유형" size="md">주문유형 탭 </LabelContent>
-      <LabelContent label="포지션" size="md">포지션 탭 </LabelContent>
-      <LabelContent label="주문일자" size="md">주문일자 영역 </LabelContent>
+      <LabelContent label="주문일자" size="md">주문일자 탭 </LabelContent>
     </div>
     <div>
       <BaseButton variant="contained" label="조회하기" size="sm" />

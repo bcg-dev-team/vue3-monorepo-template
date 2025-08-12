8f5ae78c594d4d888246bd557948590108a63202
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-12 11:06:32 +0900

    feat: 거래내역 - 주문체결  화면 퍼블리싱

--- apps/sample-desktop/src/components/layout/fragments/LoginButtonArea.vue
+++ apps/sample-desktop/src/components/layout/fragments/LoginButtonArea.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center gap-2">
-    <BaseButton variant="contained" label="로그인" size="sm" color="cancel" pill />
+    <BaseButton variant="contained" label="로그인" size="sm" color="grey" pill />
     <BaseButton variant="contained" label="회원가입" size="sm" color="primary" pill />
   </div>
 </template>

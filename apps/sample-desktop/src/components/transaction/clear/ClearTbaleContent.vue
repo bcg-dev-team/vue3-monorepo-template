ef4fc37d996d43ef20ef936170660c55a144a5f1
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-12 11:35:46 +0900

    feat: 거래내역 - 결제내역 화면 퍼블리싱

--- apps/sample-desktop/src/components/transaction/clear/ClearTbaleContent.vue
+++ apps/sample-desktop/src/components/transaction/clear/ClearTbaleContent.vue
@@ -11,7 +11,7 @@
       </LabelContent>
     </div>
     <div>
-      <LabelContent label="상세내역역" size="lg">
+      <LabelContent label="상세내역" size="lg">
         <BaseTable
           :headers="detailHeaders"
           :data="detailData"

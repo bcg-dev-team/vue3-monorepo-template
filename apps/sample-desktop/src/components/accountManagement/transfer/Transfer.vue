3aee9d0c940a7b124c297d3d00ec0226dbaa6aee
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-13 18:05:39 +0900

    feat: 계좌관리 - 출금/이체/입금 화면
    - grdient style 추가 (gradients.scss)
      - red | blue | green -> 오른쪽 하단으로 그라데이션 처리
      - red | blue | green -> 오른쪽으로 그라데이션 처리

---
+++ apps/sample-desktop/src/components/accountManagement/transfer/Transfer.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>Transfer</div>
+</template>
+<script setup lang="ts"></script>
---
+++ apps/sample-desktop/src/components/accountManagement/transfer/TransferMenuButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    :class="
+      borderColorClasses[props.color] +
+      ' p-padding-36 flex w-[320px] cursor-pointer flex-col items-center justify-center rounded-[10px] border'
+    "
+  >
+    <div :class="colorClasses[props.color] + ' p-padding-16 rounded-[10px]'">
+      <BaseIcon :name="props.icon" size="md" color="white" />
+    </div>
+    <div class="mt-size-24 gap-size-12 flex flex-col text-center">
+      <span class="text-font-18 font-semibold">{{ props.title }}</span>
+      <span class="text-font-14" v-html="props.description"></span>
+    </div>
+    <div
+      class="p-padding-16 bg-bg-bg-innerframe mt-size-24 gap-size-12 flex w-full flex-col rounded-md"
+    >
+      <div
+        class="gap-size-4 text-default-muted-dark flex items-center"
+        v-for="(info, index) in props.info"
+        :key="index"
+      >
+        <BaseIcon :name="info.icon" size="sm" color="gray" />
+        <span class="text-font-13">{{ info.text }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { BaseIcon, type IconName } from '@template/ui';
+import { computed } from 'vue';
+
+const props = defineProps<{
+  title: string;
+  description: string;
+  icon: IconName;
+  color: 'blue' | 'green' | 'red';
+  info: Array<{
+    icon: IconName;
+    text: string;
+  }>;
+}>();
+
+const colorClasses = computed(() => ({
+  blue: 'bg-gradient-to-br-blue',
+  green: 'bg-gradient-to-br-green',
+  red: 'bg-gradient-to-br-red',
+}));
+
+const borderColorClasses = computed(() => ({
+  blue: 'border-blue-blue800-deep',
+  green: 'border-green-green800',
+  red: 'border-red-red800',
+}));
+</script>

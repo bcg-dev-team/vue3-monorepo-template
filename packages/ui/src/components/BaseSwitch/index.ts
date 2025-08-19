2f7864339206e56ef9ed8677b475025dd6088a65
bcg-ygpark <ygpark@blockchainglobal.co.kr>
2025-08-19 17:55:22 +0900

    chore(packages/ui): fix style and rename directory

--- packages/ui/src/components/BaseSwitch/index.ts
+++ packages/ui/src/components/BaseSwitch/index.ts
@@ -1,2 +1,2 @@
 export { default as BaseSwitch } from './BaseSwitch.vue';
-export type { default as BaseSwitchProps } from './BaseSwitch.vue'; 
+export type { default as BaseSwitchProps } from './BaseSwitch.vue';
--- packages/ui/src/components/BaseSwitch/stories/BaseSwitch.stories.ts
+++ packages/ui/src/components/BaseSwitch/stories/BaseSwitch.stories.ts
@@ -1,5 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/vue3';
 import BaseSwitch from '../BaseSwitch/BaseSwitch.vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
 
 const meta: Meta<typeof BaseSwitch> = {
   title: 'Inputs/Switch',
@@ -188,4 +188,4 @@ export const InteractiveDemo: Story = {
       </div>
     `,
   }),
-}; 
+};

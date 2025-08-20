9eac4371f0b72a9d10111b54776631a60bb0eb60
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-21 08:51:56 +0900

    feat: 법인 회원가입 공통 컴포넌트 적용

--- apps/sample-desktop/src/components/auth/signup/corporate/CorporateDocument.vue
+++ apps/sample-desktop/src/components/auth/signup/corporate/CorporateDocument.vue
@@ -1,47 +1,30 @@
 <template>
-  <AuthContent title="법인회원 가입" :description="description">
-    <template #header>
-      <div class="flex items-center justify-between">
-        <BaseIcon name="arrow-backward" size="md" />
-        <BaseStepper variant="dot" :dot-config="{ count: 4 }" :current="3" />
-      </div>
-    </template>
-    <template #content>
-      <div class="gap-size-16 flex flex-col">
-        <FormField label="사업자등록증명원">
-          <BaseFileUploadButton status="hover" />
-        </FormField>
-        <FormField label="법인대표초본(영문)">
-          <BaseFileUploadButton status="hover" />
-        </FormField>
-        <FormField label="법인명 공과금 납부서">
-          <BaseFileUploadButton status="hover" />
-        </FormField>
+  <div class="gap-size-16 flex flex-col">
+    <FormField label="사업자등록증명원">
+      <BaseFileUploadButton status="hover" />
+    </FormField>
+    <FormField label="법인대표초본(영문)">
+      <BaseFileUploadButton status="hover" />
+    </FormField>
+    <FormField label="법인명 공과금 납부서">
+      <BaseFileUploadButton status="hover" />
+    </FormField>
 
-        <div class="mt-[33px]">
-          <BaseButton
-            size="lg"
-            label="다음"
-            variant="contained"
-            color="primary"
-            full-width
-            @click="step = 7"
-          />
-        </div>
-      </div>
-    </template>
-  </AuthContent>
+    <div class="mt-[33px]">
+      <BaseButton
+        size="lg"
+        label="다음"
+        variant="contained"
+        color="primary"
+        full-width
+        @click="step = 7"
+      />
+    </div>
+  </div>
 </template>
 <script lang="ts" setup>
-import { BaseIcon, BaseStepper, BaseButton, BaseFileUploadButton } from '@template/ui';
-import AuthContent from '@/components/auth/common/AuthContent.vue';
+import { BaseButton, BaseFileUploadButton } from '@template/ui';
 import FormField from '@/components/auth/common/FormField.vue';
 
 const step = defineModel<number>('step', { required: true });
-
-const description = `
-      구비서류의 스캔본을 제출해주세요.<br/>
-      관리자 확인 후 <span class='text-red font-medium'>승인 알림</span>을 보내드립니다.
-      <p class='mt-size-12 text-font-12 text-blue'>&#8251; 파일형식: .png, .jpg, .pdf / 파일 사이즈 300KB 미만</p>
-      `;
 </script>
--- apps/sample-desktop/src/components/auth/signup/corporate/CorporateEmailForm.vue
+++ apps/sample-desktop/src/components/auth/signup/corporate/CorporateEmailForm.vue
@@ -1,42 +1,29 @@
 <template>
-  <AuthContent title="법인회원 가입" description="아이디로 사용할 이메일주소를 입력해주세요.">
-    <template #header>
-      <div class="flex items-center justify-between">
-        <BaseIcon name="arrow-backward" size="md" />
-        <BaseStepper variant="dot" :dot-config="{ count: 4 }" :current="2" />
-      </div>
-    </template>
-    <template #content>
-      <div>
-        <div class="gap-size-16 flex flex-col">
-          <FormField label="이메일주소(ID)">
-            <BaseInput
-              size="md"
-              placeholder="example@email.com"
-              errorMessage="이메일 형식이 올바르지 않아요"
-              :error="true"
-            />
-          </FormField>
-        </div>
-        <div class="mt-[33px]">
-          <BaseButton
-            size="lg"
-            label="다음"
-            variant="contained"
-            color="primary"
-            full-width
-            @click="step = 3"
-          />
-        </div>
-      </div>
-    </template>
-  </AuthContent>
+  <div class="gap-size-16 flex flex-col">
+    <FormField label="이메일주소(ID)">
+      <BaseInput
+        size="md"
+        placeholder="example@email.com"
+        errorMessage="이메일 형식이 올바르지 않아요"
+        :error="true"
+      />
+    </FormField>
+  </div>
+  <div class="mt-[33px]">
+    <BaseButton
+      size="lg"
+      label="다음"
+      variant="contained"
+      color="primary"
+      full-width
+      @click="step = 3"
+    />
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { BaseIcon, BaseStepper, BaseInput, BaseButton } from '@template/ui';
-import AuthContent from '@/components/auth/common/AuthContent.vue';
 import FormField from '@/components/auth/common/FormField.vue';
+import { BaseInput, BaseButton } from '@template/ui';
 
 const step = defineModel<number>('step', { required: true });
 </script>

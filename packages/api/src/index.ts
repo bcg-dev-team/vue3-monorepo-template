167c545ae1a46af4abdb2ce1165d77cdc8753d1d
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-20 11:02:51 +0900

    feat(api): 기존에 정의되어있던 api client 제거

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,17 +1,4 @@
 // API 관련 기능들을 export
-export * from './client';
 export * from './services';
 export * from './types';
 export * from './plugin/axios';
-
-// 기본 API 클라이언트 인스턴스
-// import { ApiClient } from './client';
-
-/**
- * 기본 API 클라이언트 인스턴스
- */
-// export const apiClient = new ApiClient({
-//   baseURL: process.env.VITE_API_BASE_URL || 'http://localhost:3000',
-// });
-
-// export default apiClient;

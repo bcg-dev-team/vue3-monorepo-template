dea71bea54e5ede5260554fa4feb37b7719c5ff6
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-21 09:14:13 +0900

    feat: 아이디찾기 공통 컴포넌트 적용

--- apps/sample-desktop/src/components/auth/findId/AuthFindIdResult.vue
+++ apps/sample-desktop/src/components/auth/findId/AuthFindIdResult.vue
@@ -1,52 +1,35 @@
 <template>
-  <AuthContent title="아이디 찾기 완료" description="입력하신 정보와 일치하는 아이디를 찾았어요">
-    <template #header>
-      <div class="flex items-center justify-between">
-        <BaseIcon name="arrow-backward" size="md" />
-        <BaseStepper variant="dot" :dot-config="{ count: 4 }" :current="1" />
-      </div>
-    </template>
-    <template #content>
-      <div>
-        <div class="gap-size-24 flex flex-col">
-          <div class="gap-size-4 flex flex-col">
-            <span class="text-font-14 font-medium">검색 정보</span>
-            <div class="bg-bg-bg-innerframe gap-size-10 p-size-16 flex flex-col rounded-md">
-              <div class="flex items-center justify-between">
-                <span class="text-font-14 font-normal">이름</span>
-                <span class="text-font-14 font-medium">김트레이더</span>
-              </div>
-              <div class="flex items-center justify-between">
-                <span class="text-font-14 font-normal">휴대폰번호</span>
-                <span class="text-font-14 font-medium">010-1234-5678</span>
-              </div>
-            </div>
+  <div>
+    <div class="gap-size-24 flex flex-col">
+      <div class="gap-size-4 flex flex-col">
+        <span class="text-font-14 font-medium">검색 정보</span>
+        <div class="bg-bg-bg-innerframe gap-size-10 p-size-16 flex flex-col rounded-md">
+          <div class="flex items-center justify-between">
+            <span class="text-font-14 font-normal">이름</span>
+            <span class="text-font-14 font-medium">김트레이더</span>
           </div>
-          <div class="gap-size-4 flex flex-col">
-            <span class="text-font-14 font-medium">찾은 아이디(이메일주소)</span>
-            <div class="bg-bg-bg-innerframe gap-size-10 p-size-16 flex flex-col rounded-md">
-              <div class="flex items-center justify-between">
-                <span class="text-font-16 font-medium">traderkim@email.com</span>
-                <span class="text-font-16 font-medium">:copy:</span>
-              </div>
-            </div>
+          <div class="flex items-center justify-between">
+            <span class="text-font-14 font-normal">휴대폰번호</span>
+            <span class="text-font-14 font-medium">010-1234-5678</span>
           </div>
         </div>
-        <div class="gap-size-8 mt-[33px] flex w-full items-center justify-center">
-          <BaseButton
-            size="lg"
-            label="비밀번호 재설정"
-            variant="outlined"
-            color="primary"
-            full-width
-          />
-          <BaseButton size="lg" label="로그인" variant="contained" color="primary" full-width />
+      </div>
+      <div class="gap-size-4 flex flex-col">
+        <span class="text-font-14 font-medium">찾은 아이디(이메일주소)</span>
+        <div class="bg-bg-bg-innerframe gap-size-10 p-size-16 flex flex-col rounded-md">
+          <div class="flex items-center justify-between">
+            <span class="text-font-16 font-medium">traderkim@email.com</span>
+            <span class="text-font-16 font-medium">:copy:</span>
+          </div>
         </div>
       </div>
-    </template>
-  </AuthContent>
+    </div>
+    <div class="gap-size-8 mt-[33px] flex w-full items-center justify-center">
+      <BaseButton size="lg" label="비밀번호 재설정" variant="outlined" color="primary" full-width />
+      <BaseButton size="lg" label="로그인" variant="contained" color="primary" full-width />
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
-import AuthContent from '@/components/auth/common/AuthContent.vue';
-import { BaseButton, BaseIcon, BaseStepper } from '@template/ui';
+import { BaseButton } from '@template/ui';
 </script>
--- apps/sample-desktop/src/views/auth/findId/Index.vue
+++ apps/sample-desktop/src/views/auth/findId/Index.vue
@@ -1,12 +1,40 @@
 <template>
-  <AuthFindId v-if="step === 0" v-model:step="step" />
-  <AuthFindIdResult v-if="step === 1" />
+  <AuthContent :title="findIdTitle[step].title" :description="findIdTitle[step].description">
+    <template #header>
+      <div class="flex items-center justify-between">
+        <BaseIcon name="arrow-backward" size="md" />
+        <BaseStepper variant="dot" :dot-config="{ count: 2 }" :current="step" />
+      </div>
+    </template>
+    <template #content>
+      <AuthFindId v-if="step === 0" v-model:step="step" />
+      <AuthFindIdResult v-if="step === 1" />
+    </template>
+  </AuthContent>
 </template>
 
 <script lang="ts" setup>
 import AuthFindIdResult from '@/components/auth/findId/AuthFindIdResult.vue';
+import AuthContent from '@/components/auth/common/AuthContent.vue';
 import AuthFindId from '@/components/auth/findId/AuthFindId.vue';
+import { BaseIcon, BaseStepper } from '@template/ui';
 import { ref } from 'vue';
 
 const step = ref(0);
+
+type FindIdTitleProps = {
+  title?: string;
+  description?: string;
+};
+
+const findIdTitle: FindIdTitleProps[] = [
+  {
+    title: '아이디 찾기',
+    description: '회원정보에 등록된 정보를 입력해주세요',
+  },
+  {
+    title: '아이디 찾기 완료',
+    description: '입력하신 정보와 일치하는 아이디를 찾았어요',
+  },
+];
 </script>

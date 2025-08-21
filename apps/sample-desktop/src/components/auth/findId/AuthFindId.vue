5c163e332d275d0dafb042dcf0f92f1fb6ea30b2
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-21 09:14:13 +0900

    feat: 아이디찾기 공통 컴포넌트 적용

--- apps/sample-desktop/src/components/auth/findId/AuthFindId.vue
+++ apps/sample-desktop/src/components/auth/findId/AuthFindId.vue
@@ -1,50 +1,37 @@
 <template>
-  <AuthContent title="아이디 찾기" description="회원정보에 등록된 정보를 입력해주세요">
-    <template #header>
-      <div class="flex items-center justify-between">
-        <BaseIcon name="arrow-backward" size="md" />
-        <BaseStepper variant="dot" :dot-config="{ count: 4 }" :current="0" />
+  <div class="gap-size-16 flex flex-col">
+    <FormField label="이름">
+      <BaseInput size="md" />
+    </FormField>
+    <FormField label="휴대폰 번호">
+      <div class="gap-size-8 flex items-center justify-between">
+        <BaseInput size="md" placeholder="'-' 없이 입력" />
+        <BaseButton size="lg" label="인증번호 발송" variant="contained" color="primary" />
       </div>
-    </template>
-    <template #content>
-      <div>
-        <div class="gap-size-16 flex flex-col">
-          <FormField label="이름">
-            <BaseInput size="md" />
-          </FormField>
-          <FormField label="휴대폰 번호">
-            <div class="gap-size-8 flex items-center justify-between">
-              <BaseInput size="md" placeholder="'-' 없이 입력" />
-              <BaseButton size="lg" label="인증번호 발송" variant="contained" color="primary" />
-            </div>
-          </FormField>
-          <FormField label="인증번호">
-            <BaseInput
-              size="md"
-              :error="true"
-              :errorMessage="'입력시간이 만료됐어요. 인증번호를 다시 발송해주세요.'"
-            />
-          </FormField>
-        </div>
-        <div class="mt-[33px]">
-          <BaseButton
-            size="lg"
-            label="다음"
-            variant="contained"
-            color="primary"
-            full-width
-            @click="step = 1"
-          />
-        </div>
-      </div>
-    </template>
-  </AuthContent>
+    </FormField>
+    <FormField label="인증번호">
+      <BaseInput
+        size="md"
+        :error="true"
+        :errorMessage="'입력시간이 만료됐어요. 인증번호를 다시 발송해주세요.'"
+      />
+    </FormField>
+  </div>
+  <div class="mt-[33px]">
+    <BaseButton
+      size="lg"
+      label="다음"
+      variant="contained"
+      color="primary"
+      full-width
+      @click="step = 1"
+    />
+  </div>
 </template>
 
-<script lang="ts" setup>
-import { BaseIcon, BaseInput, BaseButton, BaseStepper } from '@template/ui';
-import AuthContent from '@/components/auth/common/AuthContent.vue';
+<script setup lang="ts">
 import FormField from '@/components/auth/common/FormField.vue';
+import { BaseInput, BaseButton } from '@template/ui';
 
 const step = defineModel<number>('step', { required: true });
 </script>

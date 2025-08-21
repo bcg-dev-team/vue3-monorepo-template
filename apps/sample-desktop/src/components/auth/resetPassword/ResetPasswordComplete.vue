9d48d796886f1904327c3e42483681399934f1f4
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-21 09:32:42 +0900

    feat: 비밀번호 재설정 화면 공통 컴포넌트 적용

--- apps/sample-desktop/src/components/auth/resetPassword/ResetPasswordComplete.vue
+++ apps/sample-desktop/src/components/auth/resetPassword/ResetPasswordComplete.vue
@@ -1,21 +1,16 @@
 <template>
-  <AuthContent class="pb-[30px]">
-    <template #content>
-      <div class="flex flex-col items-center justify-center">
-        <div>
-          <span class="text-font-24 font-semibold">비밀번호 재설정 완료</span>
-        </div>
-        <div class="mt-size-24 text-font-16 text-center">
-          <p>비밀번호 재설징이 완료 됐어요</p>
-          <p>다시 로그인해주세요</p>
-        </div>
-      </div>
-      <div><BaseButton label="확인" full-width /></div>
-    </template>
-  </AuthContent>
+  <div class="flex flex-col items-center justify-center">
+    <div>
+      <span class="text-font-24 font-semibold">비밀번호 재설정 완료</span>
+    </div>
+    <div class="mt-size-24 text-font-16 text-center">
+      <p>비밀번호 재설징이 완료 됐어요</p>
+      <p>다시 로그인해주세요</p>
+    </div>
+  </div>
+  <div class="mt-size-48"><BaseButton label="확인" full-width /></div>
 </template>
 
 <script lang="ts" setup>
-import AuthContent from '@/components/auth/common/AuthContent.vue';
 import { BaseButton } from '@template/ui';
 </script>
--- apps/sample-desktop/src/components/auth/resetPassword/ResetPasswordNewPassword.vue
+++ apps/sample-desktop/src/components/auth/resetPassword/ResetPasswordNewPassword.vue
@@ -1,58 +1,52 @@
 <template>
-  <AuthContent title="새 비밀번호 설정" description="새로운 비밀번호를 설정하세요">
-    <template #header>
-      <div class="flex items-center justify-between">
-        <BaseIcon name="arrow-backward" size="md" />
-        <BaseStepper variant="dot" :dot-config="{ count: 3 }" :current="2" />
-      </div>
-    </template>
-    <template #content>
-      <div>
-        <div class="gap-size-16 flex flex-col">
-          <FormField label="비밀번호">
-            <BaseInput
-              size="md"
-              placeholder="영문+숫자, 8~16자리 이상"
-              :error="true"
-              errorMessage="에러메시지"
-            />
-            <BaseProgressBar :strength-score="4" variant="password-strength" :show-label="true" />
-          </FormField>
-          <div
-            class="mt-size-12 border-primary-primary800 bg-primary-primary100 p-size-8 text-font-12 text-default-muted-dark rounded-md border"
-          >
-            <p>8자 이상 입력해주세요</p>
-            <p>영문 대/소문자, 숫자, 특수문자를 포함해주세요</p>
-            <p>연속된 문자나 반복 문자는 피해주세요</p>
-          </div>
-          <FormField label="비밀번호 확인">
-            <BaseInput
-              size="md"
-              placeholder="다시 입력해주세요"
-              :error="true"
-              errorMessage="비밀번호가 일치하지 않아요"
-            />
-          </FormField>
-        </div>
-        <div class="gap-size-12 mt-[33px] flex items-center">
-          <BaseButton
-            size="lg"
-            label="다음"
-            variant="contained"
-            color="primary"
-            full-width
-            @click="step = 3"
-          />
-        </div>
-      </div>
-    </template>
-  </AuthContent>
+  <div class="gap-size-16 flex flex-col">
+    <FormField label="비밀번호">
+      <BaseInput
+        size="md"
+        placeholder="영문+숫자, 8~16자리 이상"
+        :error="true"
+        errorMessage="에러메시지"
+      />
+      <BaseProgressBar :strength-score="4" variant="password-strength" :show-label="true" />
+    </FormField>
+    <div
+      class="mt-size-12 border-primary-primary800 bg-primary-primary100 p-size-8 text-font-12 text-default-muted-dark rounded-md border"
+    >
+      <p>8자 이상 입력해주세요</p>
+      <p>영문 대/소문자, 숫자, 특수문자를 포함해주세요</p>
+      <p>연속된 문자나 반복 문자는 피해주세요</p>
+    </div>
+    <FormField label="비밀번호 확인">
+      <BaseInput
+        size="md"
+        placeholder="다시 입력해주세요"
+        :error="true"
+        errorMessage="비밀번호가 일치하지 않아요"
+      />
+    </FormField>
+  </div>
+  <div class="gap-size-12 mt-[33px] flex items-center">
+    <BaseButton
+      size="lg"
+      label="다음"
+      variant="contained"
+      color="primary"
+      full-width
+      @click="handleSubmit"
+    />
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { BaseIcon, BaseStepper, BaseInput, BaseButton, BaseProgressBar } from '@template/ui';
-import AuthContent from '@/components/auth/common/AuthContent.vue';
+import { BaseInput, BaseButton, BaseProgressBar } from '@template/ui';
 import FormField from '@/components/auth/common/FormField.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const step = defineModel<number>('step', { required: true });
+
+const handleSubmit = () => {
+  router.replace({ name: 'reset-password-complete' });
+};
 </script>

2039aca0ed745ecfc14656edbe13346da8e51cf7
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-26 08:55:33 +0900

    refactor: 계좌관리 > 출금/이체/입금 화면 컨텐츠 공통화

--- apps/sample-desktop/src/components/accountManagement/service/AccountServiceMain.vue
+++ apps/sample-desktop/src/components/accountManagement/service/AccountServiceMain.vue
@@ -6,10 +6,6 @@
           <BaseStepper :labelConfig="steps" :current="step" variant="label" />
         </div>
         <div class="gap-size-36 flex flex-col items-center justify-center">
-          <div class="gap-size-8 flex flex-col items-center justify-center">
-            <span class="text-font-24 font-semibold">{{ menuTitle.title }}</span>
-            <span class="text-font-14 text-default-muted-dark">{{ menuTitle.subTitle }}</span>
-          </div>
           <template v-if="step === 0">
             <SelectService @menuClick="handleMenuClick" />
           </template>
@@ -32,12 +28,7 @@ import SelectService from '@/components/accountManagement/service/content/Select
 import type { TransferMenuState } from '@/components/accountManagement/accountManagement';
 import MainCardContent from '@/components/common/cards/MainCardContent.vue';
 import { BaseStepper } from '@template/ui';
-import { ref, computed } from 'vue';
-
-interface TransferMenuTitle {
-  title: string;
-  subTitle: string;
-}
+import { ref } from 'vue';
 
 const step = ref(0);
 const menuState = ref<TransferMenuState | null>(null);
@@ -57,33 +48,4 @@ const handleMenuClick = (menu: string) => {
 const handleStepChange = (newStep: number) => {
   step.value = newStep;
 };
-
-const menuTitle = computed<TransferMenuTitle>(() => {
-  if (menuState.value === 'withdraw') {
-    return {
-      title: '출금신청 금액 입력',
-      subTitle: '출금 신청하실 금액을 입력해주세요',
-    };
-  } else if (menuState.value === 'transfer') {
-    return {
-      title: '이체금액 입력',
-      subTitle: '이체하실 계좌와 금액을 입력해주세요',
-    };
-  } else if (menuState.value === 'deposit') {
-    return {
-      title: '입금정보 입력',
-      subTitle: '입금하신 정보를 정확히 입력해주세요',
-    };
-  } else if (menuState.value === 'complete') {
-    return {
-      title: '완료되었습니다.',
-      subTitle: '영업일 기준 1~3일 소요될 수 있습니다.',
-    };
-  } else {
-    return {
-      title: '유형 선택',
-      subTitle: '입금과 출금, 이체 중 원하는 유형을 선택해주세요',
-    };
-  }
-});
 </script>
--- apps/sample-desktop/src/components/accountManagement/service/content/SelectService.vue
+++ apps/sample-desktop/src/components/accountManagement/service/content/SelectService.vue
@@ -1,4 +1,10 @@
 <template>
+  <div class="gap-size-8 flex flex-col items-center justify-center">
+    <span class="text-font-24 font-semibold">유형 선택</span>
+    <span class="text-font-14 text-default-muted-dark">
+      입금과 출금, 이체 중 원하는 입출금 유형을 선택해주세요
+    </span>
+  </div>
   <div class="gap-size-24 flex">
     <TransferMenuButton
       v-for="menu in menuList"

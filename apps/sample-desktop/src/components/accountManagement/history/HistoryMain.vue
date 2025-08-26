714f2015e5335c44f04168db3fdf8019545e9cce
pcjin <pcjin@blockchainglobal.co.kr>
2025-08-26 10:02:52 +0900

    feat: 계좌 관리 > 입출금내역 화면 퍼블리싱싱

---
+++ apps/sample-desktop/src/components/accountManagement/history/HistoryMain.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="gap-size-24 gap-size-16 flex">
+    <MainCardContent class="h-[689px] w-[300px] flex-shrink-0 p-6" title="검색 및 필터" size="sm">
+      <template #content>
+        <div class="gap-size-16 flex flex-col">
+          <LabelContent label="선택 계좌" size="custom" class="text-font-13">
+            <template #content>
+              <BaseInput size="sm" type="text" placeholder="계좌를 선택해주세요" />
+            </template>
+          </LabelContent>
+          <LabelContent label="선택 계좌" size="custom" class="text-font-13">
+            <template #content>
+              <BaseInput size="sm" type="text" placeholder="계좌를 선택해주세요" />
+            </template>
+          </LabelContent>
+          <LabelContent label="선택 계좌" size="custom" class="text-font-13">
+            <template #content>
+              <BaseInput size="sm" type="text" placeholder="계좌를 선택해주세요" />
+            </template>
+          </LabelContent>
+        </div>
+        <div class="mt-size-24">
+          <BaseButton
+            variant="outlined"
+            :leftIcon="{ name: 'refresh', size: 'md', color: 'var(--input-icon-off-dark)' }"
+            size="md"
+            color="white"
+            label="필터 초기화"
+            fullWidth
+          />
+        </div>
+      </template>
+    </MainCardContent>
+    <MainCardContent class="w-full p-6" title="거래내역" size="sm"></MainCardContent>
+  </div>
+</template>
+<script setup lang="ts">
+import MainCardContent from '@/components/common/cards/MainCardContent.vue';
+import LabelContent from '@/components/common/LabelContent.vue';
+import { BaseButton, BaseInput } from '@template/ui';
+</script>

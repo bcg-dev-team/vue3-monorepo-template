67c781f4b39ebe4c4f7dcd8d4b8303639e12d99d
pcjin <pcjin@blockchainglobal.co.kr>
2025-09-01 14:26:18 +0900

    feat: 1:1문의 table관련 공통화
    - tablelabel 컴포넌트 조합해서 사용하는 형식
    - 추후 tablerow  관련 컴포넌트도 생성이 필요할지 고민 필요

--- apps/sample-desktop/src/components/support/qna/QnaRegister.vue
+++ apps/sample-desktop/src/components/support/qna/QnaRegister.vue
@@ -4,53 +4,48 @@
       <span class="text-font-20 font-semibold">문의 내용</span>
     </div>
     <div class="mt-size-8">
-      <table class="qna-table">
-        <tr>
-          <th>제목</th>
-          <td class="!py-2" colspan="3">
-            <BaseInput class="w-[600px]" size="sm" placeholder="제목을 입력하세요." />
-          </td>
-        </tr>
-        <tr>
-          <th>카테고리</th>
-          <td class="!py-2" colspan="3">
-            <BaseInput class="w-[240px]" size="sm" placeholder="셀렉트 박스가 들어갈 예정이에요" />
-          </td>
-        </tr>
-        <tr>
-          <th>내용</th>
-          <td class="qna-content !py-2" colspan="3">
-            <div class="min-h-[516px]">
-              <BaseInput size="sm" placeholder="Text Area가 들어갈 예정이에요" />
+      <div class="border-t border-[var(--table-type2-body-border)]">
+        <TableLabel title="제목">
+          <BaseInput class="w-[600px]" size="sm" placeholder="제목을 입력하세요." />
+        </TableLabel>
+      </div>
+      <div class="border-t border-[var(--table-type2-body-border)]">
+        <TableLabel title="카테고리">
+          <BaseInput class="w-[240px]" size="sm" placeholder="셀렉트 박스가 들어갈 예정이에요" />
+        </TableLabel>
+      </div>
+      <div class="border-t border-[var(--table-type2-body-border)]">
+        <TableLabel title="내용">
+          <div class="py-size-8 min-h-[516px] w-full">
+            <BaseInput size="sm" placeholder="Text Area가 들어갈 예정이에요" />
+          </div>
+        </TableLabel>
+      </div>
+      <div class="border-b border-t border-[var(--table-type2-body-border)]">
+        <TableLabel title="첨부파일">
+          <div class="gap-size-10 flex items-center">
+            <div>
+              <BaseFileUploadButton class="w-[130px]" status="hover" />
             </div>
-          </td>
-        </tr>
-        <tr>
-          <th>첨부파일</th>
-          <td class="!py-2" colspan="3">
-            <div class="gap-size-10 flex items-center">
-              <div>
-                <BaseFileUploadButton class="w-[130px]" status="hover" />
-              </div>
-              <div class="gap-size-4 flex items-center">
-                <Anchor @click="() => console.log('첨부파일 클릭!')">
-                  20250601_Screenshot_14.00.01.jpg
-                </Anchor>
-                <BaseIcon name="icn-delete" size="sm" color="rgba(180, 182, 187, 1)" />
-              </div>
+            <div class="gap-size-4 flex items-center">
+              <Anchor @click="() => console.log('첨부파일 클릭!')">
+                20250601_Screenshot_14.00.01.jpg
+              </Anchor>
+              <BaseIcon name="icn-delete" size="sm" color="var(--input-icon-off)" />
             </div>
-          </td>
-        </tr>
-      </table>
+          </div>
+        </TableLabel>
+      </div>
     </div>
     <div class="gap-size-12 pt-padding-24 flex justify-center">
       <BaseButton label="등록하기" size="lg" variant="contained" />
-      <BaseButton label="취소" size="lg" variant="outlined" @click="type = null" />
+      <BaseButton label="취소" size="lg" variant="outlined" color="white" @click="type = null" />
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { BaseInput, BaseIcon, BaseFileUploadButton, BaseButton } from '@template/ui';
+import TableLabel from '@/components/support/qna/common/TableLabel.vue';
 import Anchor from '@/components/common/Anchor.vue';
 
 const type = defineModel<null | 'register' | 'detail'>('type', { required: true });

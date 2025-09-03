3eeecfdf43939caf121da39f791ee7a9e829ec04
bcg-hyunwoo <hyunwoo@blockchainglobal.co.kr>
2025-09-03 10:05:24 +0900

    feat: inputselect 컴포넌트 작업

--- packages/ui/src/components/BaseInput/index.ts
+++ packages/ui/src/components/BaseInput/index.ts
@@ -1,2 +1,3 @@
 export { default as BaseInput } from './BaseInput.vue';
 export { default as BaseInputStepper } from './BaseInputStepper.vue';
+export { default as BaseInputSelect } from './BaseInputSelect.vue';
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -13,6 +13,7 @@ export { default as BaseSwitch } from './BaseSwitch/BaseSwitch.vue';
 export { default as BaseRadioGroup } from './BaseRadioGroup/BaseRadioGroup.vue';
 export { default as BaseTwoWaySplitPane } from './BaseTwoWaySplitPane/BaseTwoWaySplitPane.vue';
 export { default as BaseInputStepper } from './BaseInput/BaseInputStepper.vue';
+export { default as BaseInputSelect } from './BaseInput/BaseInputSelect.vue';
 
 export * from './BasePagination';
 export * from './BaseTable';

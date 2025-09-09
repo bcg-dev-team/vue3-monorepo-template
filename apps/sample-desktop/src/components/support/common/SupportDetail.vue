dcf66ad4807d2ce56d29bfb114bcebf3c7c3248b
pcjin <pcjin@blockchainglobal.co.kr>
2025-09-09 14:00:05 +0900

    feat(apps/desktop): 1:1 문의 다크모드 색상 적용

--- apps/sample-desktop/src/components/support/common/SupportDetail.vue
+++ apps/sample-desktop/src/components/support/common/SupportDetail.vue
@@ -2,7 +2,7 @@
   <div
     class="bg-bg-bg-innerframe border-bg-bg-outline px-padding-48 py-padding-24 min-h-[794px] w-full rounded-[10px] border"
   >
-    <div class="py-padding-16 gap-size-12 flex flex-col border-b">
+    <div class="py-padding-16 gap-size-12 border-bg-bg-outline flex flex-col border-b">
       <div class="gap-size-12 flex items-center">
         <BaseChip :label="props.chip.label" size="md" :variant="props.chip.variant" />
         <span class="text-font-13 text-default-muted">{{ props.date }}</span>

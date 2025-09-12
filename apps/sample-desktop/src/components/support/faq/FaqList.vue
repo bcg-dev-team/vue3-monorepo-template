1fc20dbcac653d0cfafa04caf650410e9f5ece4f
pcjin <pcjin@blockchainglobal.co.kr>
2025-09-12 09:45:32 +0900

    feat(packages/ui): Disclosure UI 구현
    - 기본스타일
      - 색상: 회색 | 빨강 | 파랑
      - 버튼, 패널 텍스트 지정
    - 커스텀 스타일
      - 버튼, 패널 스타일 지정 가능
      - 토글 아이콘 위치 조정
      - 토글 아이콘 여부 선택

---
+++ apps/sample-desktop/src/components/support/faq/FaqList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="mb-size-8 text-font-14">
+    총 <span class="text-red font-medium">{{ props.faqList.length }}</span> 건
+  </div>
+  <div
+    class="itmes-center text-font-12 flex border-b-2 border-[var(--table-type1-header-underline)] bg-[var(--table-type2-header-bg)] font-medium"
+  >
+    <div class="py-size-8 w-[140px] text-center">
+      <span>번호</span>
+    </div>
+    <div class="py-size-8 w-[160px] text-center">
+      <span>카테고리</span>
+    </div>
+    <div class="py-size-8 w-full text-center">
+      <span>질문</span>
+    </div>
+  </div>
+  <BaseDisclosure
+    v-for="item in props.faqList"
+    :key="item.no"
+    custom
+    :show-arrow="true"
+    :arrow-position="30"
+    :default-open="item.autoOpen"
+  >
+    <template #button>
+      <div
+        class="text-font-14 flex items-center border-b border-[var(--table-type2-body-border-mid)]"
+      >
+        <div class="py-size-12 w-[140px] text-center">
+          <span>{{ item.no }}</span>
+        </div>
+        <div class="py-size-12 w-[160px] text-center">
+          <span>{{ item.category }}</span>
+        </div>
+        <div class="py-size-12 pl-size-10 w-full">
+          <span class="font-medium">Q. {{ item.question }}</span>
+        </div>
+      </div>
+    </template>
+    <template #panel>
+      <div
+        class="py-padding-36 pl-padding-48 border-b border-[var(--table-type2-body-border-mid)] bg-[var(--table-bg-inner)]"
+      >
+        <div class="gap-size-8 flex items-start">
+          <span class="text-font-14">A.</span>
+          <div class="text-font-14 flex-1" v-html="item.answer" />
+        </div>
+      </div>
+    </template>
+  </BaseDisclosure>
+</template>
+<script setup lang="ts">
+import { BaseDisclosure } from '@template/ui';
+import type { FaqList } from './Faq.vue';
+
+const props = defineProps<{
+  faqList: FaqList[];
+}>();
+</script>

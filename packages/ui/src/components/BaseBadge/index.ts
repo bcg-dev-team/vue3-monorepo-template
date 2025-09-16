f77319d5534d883cf96745f5945dadb6d3c15345
pcjin <pcjin@blockchainglobal.co.kr>
2025-09-16 15:55:56 +0900

    feat: badge ui 설계
    - 기본 배지 속성 적용
      - max | showZero | hidden | position 등

---
+++ packages/ui/src/components/BaseBadge/index.ts
@@ -0,0 +1,14 @@
+/**
+ * BaseBadge 컴포넌트
+ * 접근성을 고려한 배지 컴포넌트
+ */
+export { default } from './BaseBadge.vue';
+export type {
+  BadgeColor,
+  BadgeVariant,
+  BadgeOverlap,
+  BadgeVertical,
+  BadgeHorizontal,
+  BadgeAnchorOrigin,
+  BadgeProps,
+} from '../../types/components';

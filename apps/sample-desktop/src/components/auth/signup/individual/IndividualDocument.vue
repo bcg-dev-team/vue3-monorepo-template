35ea4cf028cb1ea63961d4306f1b9a428dde9497
bcg-hyunwoo <hyunwoo@blockchainglobal.co.kr>
2025-09-16 13:34:46 +0900

    feat: 개인회원 서류 컴포넌트 작업

--- apps/sample-desktop/src/components/auth/signup/individual/IndividualDocument.vue
+++ apps/sample-desktop/src/components/auth/signup/individual/IndividualDocument.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-col gap-[33px]">
-    <FormField label="신분증 (주민증록증 또는 운전면허증)*">
+    <FormField label="신분증 (주민등록증 또는 운전면허증)*">
       <BaseFileUploadButton
         status="hover"
-        @file-selected="handleFileSelected"
+        @fileSelected="handleFileSelected"
         @remove="handleFileRemove"
       />
     </FormField>
@@ -17,7 +17,11 @@
           <span class="font-medium">주민등록 초본(영문)</span> 또는
           <span class="font-medium">관공서 납부영수증</span>을 제출해주세요.
         </span>
-        <BaseFileUploadButton status="hover" />
+        <BaseFileUploadButton
+          status="hover"
+          @fileSelected="handleAdditionalFileSelected"
+          @remove="handleAdditionalFileRemove"
+        />
       </div>
     </div>
     <div class="">
@@ -28,6 +32,7 @@
         color="primary"
         full-width
         @click="handleSubmit"
+        :disabled="isSubmitDisabled"
       />
     </div>
   </div>
@@ -36,33 +41,52 @@
 import { BaseButton, BaseFileUploadButton, BaseCheckbox } from '@template/ui';
 import FormField from '@/components/auth/common/FormField.vue';
 import { useSignupStore } from '@/stores/useSignupStore';
+import { userService } from '@/service/api';
 import { useRouter } from 'vue-router';
-import { ref, reactive } from 'vue';
+import { ref, computed } from 'vue';
 
 const signupStore = useSignupStore();
 const router = useRouter();
 
 const isChecked = ref<boolean>(false);
 
-const state = reactive({
-  idCard: null as File | null,
-  additionalIdDocument: null as File | null,
+const idCard = ref<File | null>(null);
+const additionalIdDocument = ref<File | null>(null);
+
+const isSubmitDisabled = computed(() => {
+  const disabled = !idCard.value || (isChecked.value && !additionalIdDocument.value);
+
+  return disabled;
 });
 
-const handleFileSelected = (file: File) => {
-  state.idCard = file;
-  console.log(file);
+const handleFileSelected = (files: File[]) => {
+  idCard.value = files[0] || null;
 };
 
 const handleFileRemove = (file: File) => {
-  state.idCard = null;
-  console.log(file);
+  idCard.value = null;
 };
 
-const handleSubmit = () => {
-  if (state.idCard) {
-    signupStore.uploadIndividualDocument(state.idCard, state.additionalIdDocument);
-    router.push({ name: 'sign-up-complete' });
-  }
+const handleAdditionalFileSelected = (files: File[]) => {
+  additionalIdDocument.value = files[0] || null;
+};
+
+const handleAdditionalFileRemove = (file: File) => {
+  additionalIdDocument.value = null;
+};
+
+const handleSubmit = async () => {
+  signupStore.uploadIndividualDocument(idCard.value!, additionalIdDocument.value);
+  console.log(signupStore.getIndividualSignupInfo);
+
+  router.push({ name: 'sign-up-complete' });
+
+  // TODO: 회원가입 api 주석 해제
+  // try {
+  //   const response = await userService.joinIndividualMember(signupStore.getIndividualSignupInfo);
+  //   router.push({ name: 'sign-up-complete' });
+  // } catch (error) {
+  //   console.error(error);
+  // }
 };
 </script>

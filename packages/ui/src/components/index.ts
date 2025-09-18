882d70cdd5e3972451465d5c5bf74ee32880319a
bcg-hyunwoo <hyunwoo@blockchainglobal.co.kr>
2025-09-18 15:27:34 +0900

    feat: 사용하지 않는 컴포넌트 제거

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -2,7 +2,6 @@ export { default as BaseButton } from './BaseButton/BaseButton.vue';
 export { default as BaseTabs } from './BaseTabs/BaseTabs.vue';
 export { default as BaseInput } from './BaseInput/BaseInput.vue';
 export { default as BaseInputText } from './BaseInput/BaseInputText.vue';
-export { default as BaseInputPassword } from './BaseInput/BaseInputPassword.vue';
 export { default as BaseProgressBar } from './BaseProgressBar/BaseProgressBar.vue';
 export { default as BasePasswordStrength } from './BasePasswordStrength/BasePasswordStrength.vue';
 export { default as BaseChip } from './BaseChips/BaseChip.vue';

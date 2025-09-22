a17d3f6cce717d4b207eff3baeb7e8319d74a368
bcg-hyunwoo <hyunwoo@blockchainglobal.co.kr>
2025-09-22 10:23:37 +0900

    design: 변경된 화면 퍼블리싱 작업

--- apps/sample-desktop/src/components/accountManagement/create/AccountInfoDetail.vue
+++ apps/sample-desktop/src/components/accountManagement/create/AccountInfoDetail.vue
@@ -17,7 +17,7 @@
             >
               <template #content>
                 <div class="mt-size-4">
-                  <span class="text-font-16 font-medium">110-81-345154</span>
+                  <span class="text-font-16 font-medium">{{ account.accountNo }}</span>
                 </div>
               </template>
             </LabelContent>
@@ -28,20 +28,27 @@
               :style="'text-font-14 text-default-muted'"
             >
               <template #content>
-                <div class="mt-size-4 flex items-center justify-between">
+                <div class="mt-size-4">
+                  <div
+                    v-if="!inputState.updateAccountName"
+                    class="flex items-center justify-between"
+                  >
+                    <span class="text-font-16 font-medium">{{ account.accountAlias }}</span>
+                    <BaseButton
+                      v-if="!inputState.updateAccountName && !inputState.updateAccountPassword"
+                      variant="chip"
+                      color="grey"
+                      label="변경하기"
+                      @click="inputState.updateAccountName = true"
+                    />
+                  </div>
                   <BaseInput
+                    v-else
                     size="sm"
                     type="text"
                     placeholder="10자 이내 입력"
                     :readonly="!inputState.updateAccountName"
-                  />
-                  <BaseButton
-                    v-if="!inputState.updateAccountName"
-                    variant="contained"
-                    size="sm"
-                    color="grey"
-                    label="변경하기"
-                    @click="inputState.updateAccountName = true"
+                    v-model="accountAlias"
                   />
                 </div>
               </template>
@@ -51,25 +58,32 @@
                 inputState.updateAccountPassword && 'pb-padding-16 border-bg-bg-outline border-b'
               "
               label="계좌 비밀번호"
+              class="pb-padding-16 border-bg-bg-outline border-b"
               size="custom"
               :style="'text-font-14 text-default-muted'"
             >
               <template #content>
-                <div class="mt-size-4 flex items-center justify-between">
+                <div class="mt-size-4">
+                  <div
+                    v-if="!inputState.updateAccountPassword"
+                    class="flex items-center justify-between"
+                  >
+                    <span class="text-font-16 font-medium">●●●●●●</span>
+                    <BaseButton
+                      v-if="!inputState.updateAccountPassword && !inputState.updateAccountName"
+                      variant="chip"
+                      color="grey"
+                      label="변경하기"
+                      @click="inputState.updateAccountPassword = true"
+                    />
+                  </div>
                   <BaseInput
-                    class="w-[140px]"
+                    v-else
                     size="sm"
                     type="text"
-                    placeholder="6자리 입력"
-                    :readonly="!inputState.updateAccountPassword"
-                  />
-                  <BaseButton
-                    v-if="!inputState.updateAccountPassword"
-                    variant="contained"
-                    size="sm"
-                    color="grey"
-                    label="변경하기"
-                    @click="inputState.updateAccountPassword = true"
+                    placeholder="10자 이내 입력"
+                    :readonly="!inputState.updateAccountName"
+                    v-model="accountPassword"
                   />
                 </div>
               </template>
@@ -83,6 +97,24 @@
               <template #content>
                 <div class="mt-size-4 flex items-center justify-between">
                   <BaseInput class="w-[140px]" size="sm" type="text" placeholder="6자리 입력" />
+                </div>
+              </template>
+            </LabelContent>
+            <LabelContent
+              v-if="!accoutInfoState"
+              :class="
+                inputState.updateAccountPassword && 'pb-padding-16 border-bg-bg-outline border-b'
+              "
+              label="활성화 여부"
+              size="custom"
+              :style="'text-font-14 text-default-muted'"
+            >
+              <template #content>
+                <div class="mt-size-4">
+                  <div class="flex items-center justify-between">
+                    <span class="text-font-16 font-medium">계좌 활성화</span>
+                    <div>토글이 들어올 영역</div>
+                  </div>
                 </div>
               </template>
             </LabelContent>
@@ -107,7 +139,17 @@
 import MainCardContent from '@/components/common/cards/MainCardContent.vue';
 import LabelContent from '@/components/common/LabelContent.vue';
 import { BaseButton, BaseInput } from '@template/ui';
-import { computed, reactive } from 'vue';
+import { accountService } from '@/service/api';
+import { computed, reactive, ref } from 'vue';
+import { AccountInfo } from '@template/api';
+
+const props = defineProps<{
+  account: AccountInfo;
+}>();
+
+const accountAlias = ref('');
+const accountPassword = ref('');
+const accountPasswordCheck = ref('');
 
 const inputState = reactive({
   updateAccountName: false,
@@ -115,6 +157,13 @@ const inputState = reactive({
 });
 
 const updateAccountInfo = () => {
+  if (inputState.updateAccountName) {
+    // accountService.updateAccountInfo(
+    //   props.account.email,
+    //   props.account.accountNo,
+    //   accountAlias.value
+    // );
+  }
   inputState.updateAccountName = false;
   inputState.updateAccountPassword = false;
 };

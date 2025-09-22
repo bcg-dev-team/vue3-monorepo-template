bcfacfeb4ba7acae97ecc25bad06927d2ec8f262
bcg-hyunwoo <hyunwoo@blockchainglobal.co.kr>
2025-09-22 10:22:54 +0900

    feat: swagger 기반 api type,function 선언

--- apps/sample-desktop/src/service/api/index.ts
+++ apps/sample-desktop/src/service/api/index.ts
@@ -1,6 +1,8 @@
+import { AccountService } from '@template/api';
 import { TermsService } from '@template/api';
 import { UserService } from '@template/api';
 import api from '@/modules/axios';
 
 export const userService = new UserService(api);
 export const termsService = new TermsService(api);
+export const accountService = new AccountService(api);

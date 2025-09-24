e93f86cb163d839c9308e8682fe3a51541cd4cb4
pcjin <pcjin@blockchainglobal.co.kr>
2025-09-24 13:58:24 +0900

    feat(apps/sample-desktop): 거래내역 api 적용
    - 주문체결: /trades
    - 청산손익: /trades/liquidation/profit-loss
    - 결제내역: /trades/payments

--- apps/sample-desktop/src/service/api/index.ts
+++ apps/sample-desktop/src/service/api/index.ts
@@ -1,6 +1,6 @@
-import { TermsService } from '@template/api';
-import { UserService } from '@template/api';
+import { TermsService, TradeService, UserService } from '@template/api';
 import api from '@/modules/axios';
 
 export const userService = new UserService(api);
 export const termsService = new TermsService(api);
+export const tradeService = new TradeService(api);
---
+++ packages/api/src/types/trade.types.ts
@@ -0,0 +1,155 @@
+import { ApiSuccessResponse } from './api.types';
+
+export interface TradeOrderListRequest {
+  accountNo: string;
+  accountPassword: string;
+  orderStartDate: string;
+  orderEndDate: string;
+  orderCd: string;
+  positionCd: string;
+  nextKey: number;
+}
+
+export interface OrderSummary {
+  tradeCurrencyCd: string;
+  longExecutionQuantity: number;
+  longExecutionPrice: number;
+  reShortExecutionQuantity: number;
+  reShortExecutionPrice: number;
+  shortExecutionQuantity: number;
+  shortExecutionPrice: number;
+  reLongExecutionQuantity: number;
+  reLongExecutionPrice: number;
+}
+
+export interface OrderDetail {
+  orderDate: string;
+  orderNo: string;
+  executionDate: string;
+  executionNo: string;
+  stockCd: string;
+  positionCd: string;
+  orderTypeCd: string;
+  sideCd: string;
+  orderQuantity: number;
+  barrierPrice: number;
+  orderPrice: number;
+  profitRealizationBarrierPrice: number;
+  lossCutBarrierPrice: number;
+  executionQuantity: number;
+  executionPrice: number;
+  orderBalance: number;
+  orderStatusCd: string;
+  receptionDate: string;
+  rejectReason: string;
+}
+
+export interface TradeOrderListData {
+  hasNext: boolean;
+  nextKey: number;
+  summary: OrderSummary[];
+  deatils: OrderDetail[];
+}
+
+export interface TradeOrderListResponse extends ApiSuccessResponse<TradeOrderListData> {}
+
+export interface TradePaymentsHistoryRequest {
+  accountNo: string;
+  accountPassword: string;
+  orderStartDate: string;
+  orderEndDate: string;
+  nextKey: number;
+}
+
+export interface TradePaymentsHistorySummary {
+  tradeCurrencyCd: string;
+  longExecutionQuantity: number;
+  longExecutionPrice: number;
+  reShortExecutionQuantity: number;
+  reShortExecutionPrice: number;
+  shortExecutionQuantity: number;
+  shortExecutionPrice: number;
+  reLongExecutionQuantity: number;
+  reLongExecutionPrice: number;
+}
+export interface TradePaymentsHistoryDetail {
+  orderDate: string;
+  orderNo: string;
+  executionDate: string;
+  executionNo: string;
+  stockCd: string;
+  positionCd: string;
+  orderTypeCd: string;
+  sideCd: string;
+  orderQuantity: number;
+  barrierPrice: number;
+  orderPrice: number;
+  profitRealizationBarrierPrice: number;
+  lossCutBarrierPrice: number;
+  executionQuantity: number;
+  executionPrice: number;
+  orderBalance: number;
+  orderStatusCd: string;
+  receptionDate: string;
+  rejectReason: string;
+}
+
+export interface TradePaymentsHistoryData {
+  hasNext: boolean;
+  nextKey: string;
+  details: TradePaymentsHistoryDetail[];
+}
+
+export interface TradePaymentsHistoryResponse
+  extends ApiSuccessResponse<TradePaymentsHistoryData> {}
+
+export interface TradeProfitAndLossRequest {
+  accountNo: string;
+  accountPassword: string;
+  orderStartDate: string;
+  orderEndDate: string;
+  nextKey: number;
+}
+
+export interface TradeProfitAndLossSummary {
+  longAcquisitionPrice: number;
+  longLiquidationPrice: number;
+  longLiquidationProfitLoss: number;
+  longLiquidationProfitPercent: number;
+  longSwapCharge: number;
+  longExecutionCharge: number;
+  longLiquidationNetProfitLoss: number;
+  longLiquidationNetProfitPercent: number;
+  shortAcquisitionPrice: number;
+  shortLiquidationPrice: number;
+  shortLiquidationProfitLoss: number;
+  shortLiquidationProfitPercent: number;
+  shortSwapCharge: number;
+  shortExecutionCharge: number;
+  shortLiquidationNetProfitLoss: number;
+  shortLiquidationNetProfitPercent: number;
+  nextKey: number;
+}
+
+export interface TradeProfitAndLossDetail {
+  liquidationDate: string;
+  symbolCd: string;
+  orderCd: string;
+  purchasePrice: number;
+  liquidationPrice: number;
+  liquidationQuantity: number;
+  liquidationProfitLoss: number;
+  swapCharge: number;
+  tradeCharge: number;
+  liquidationNetProfitLoss: number;
+  purchaseDate: string;
+}
+
+export interface TradeProfitAndLossData {
+  hasNext: boolean;
+  nextKey: number;
+  summary: TradeProfitAndLossSummary;
+  details: TradeProfitAndLossDetail[];
+}
+
+export interface TradeProfitAndLossResponse extends ApiSuccessResponse<TradeProfitAndLossData> {}

842bcc0c3a224e8a8d82ed867b89dc05d714368d
pcjin <pcjin@blockchainglobal.co.kr>
2025-09-24 13:59:49 +0900

    feat(apps/sample-dsektop): 거래내역 검색박스 화면 작업
    - 일자 선택 공통 처리 -> PeriodSelect.vue
    - 디자인 일부 수정
    - BaseInputCalander scss 오버라이드해서 사용

---
+++ apps/sample-desktop/src/components/transaction/constants/searchBoxCod.ts
@@ -0,0 +1,11 @@
+export const POSITION_CODE = {
+  total: 'TOTAL',
+  long: 'LONG',
+  short: 'SHORT',
+} as const;
+
+export const ORDER_CODE = {
+  total: 'TOTAL',
+  buy: 'BUY_PURCHASE',
+  clear: 'SELL_PURCHASE',
+} as const;

fd9a4c22d2648a70e4203d4d8bfabdd0c6be243a
bcg-hyunwoo <hyunwoo@blockchainglobal.co.kr>
2025-09-30 17:19:22 +0900

    design: 5단계 호가 수정

--- apps/sample-desktop/src/components/order/OrderBook.vue
+++ apps/sample-desktop/src/components/order/OrderBook.vue
@@ -13,21 +13,21 @@
       </div>
       <div class="flex w-full items-center justify-between text-xs">
         <div class="relative">
-          <div class="relative z-10 ml-[9px]">1.00</div>
+          <div class="relative z-10 ml-[9px]">1.16790</div>
           <div
             class="absolute left-0 top-1/2 h-6 w-[5px] -translate-y-1/2 rounded bg-[#FFDBDC]"
           ></div>
         </div>
-        <div class="relative z-10">1.16790</div>
+        <div class="relative z-10">1.00</div>
       </div>
     </div>
 
     <!-- 2단계 호가 -->
     <div class="flex h-6 w-full gap-1">
       <div class="flex h-6 w-full items-center justify-between text-xs">
-        <div class="relative z-10">1.00</div>
+        <div class="relative z-10">5.00</div>
         <div class="relative">
-          <div class="relative z-10 mr-[9px]">1.16790</div>
+          <div class="relative z-10 mr-[9px]">1.35064</div>
           <div
             class="absolute right-0 top-1/2 h-6 w-[40px] -translate-y-1/2 rounded bg-[#E0EDFF]"
           ></div>
@@ -35,21 +35,21 @@
       </div>
       <div class="flex w-full items-center justify-between text-xs">
         <div class="relative">
-          <div class="relative z-10 ml-[9px]">1.00</div>
+          <div class="relative z-10 ml-[9px]">1.35076</div>
           <div
             class="absolute left-0 top-1/2 h-6 w-[40px] -translate-y-1/2 rounded bg-[#FFDBDC]"
           ></div>
         </div>
-        <div class="relative z-10">1.16790</div>
+        <div class="relative z-10">5.00</div>
       </div>
     </div>
 
     <!-- 3단계 호가 -->
     <div class="flex h-6 w-full gap-1">
       <div class="flex h-6 w-full items-center justify-between text-xs">
-        <div class="relative z-10">1.00</div>
+        <div class="relative z-10">10.00</div>
         <div class="relative">
-          <div class="relative z-10 mr-[9px]">1.16790</div>
+          <div class="relative z-10 mr-[9px]">1.35064</div>
           <div
             class="absolute right-0 top-1/2 h-6 w-[60px] -translate-y-1/2 rounded bg-[#E0EDFF]"
           ></div>
@@ -57,21 +57,21 @@
       </div>
       <div class="flex w-full items-center justify-between text-xs">
         <div class="relative">
-          <div class="relative z-10 ml-[9px]">1.00</div>
+          <div class="relative z-10 ml-[9px]">1.35090</div>
           <div
             class="absolute left-0 top-1/2 h-6 w-[60px] -translate-y-1/2 rounded bg-[#FFDBDC]"
           ></div>
         </div>
-        <div class="relative z-10">1.16790</div>
+        <div class="relative z-10">10.00</div>
       </div>
     </div>
 
     <!-- 4단계 호가 -->
     <div class="flex h-6 w-full gap-1">
       <div class="flex h-6 w-full items-center justify-between text-xs">
-        <div class="relative z-10">1.00</div>
+        <div class="relative z-10">30.00</div>
         <div class="relative">
-          <div class="relative z-10 mr-[9px]">1.16790</div>
+          <div class="relative z-10 mr-[9px]">1.35071</div>
           <div
             class="absolute right-0 top-1/2 h-6 w-[100px] -translate-y-1/2 rounded bg-[#E0EDFF]"
           ></div>
@@ -79,21 +79,21 @@
       </div>
       <div class="flex w-full items-center justify-between text-xs">
         <div class="relative">
-          <div class="relative z-10 ml-[9px]">1.00</div>
+          <div class="relative z-10 ml-[9px]">1.35096</div>
           <div
             class="absolute left-0 top-1/2 h-6 w-[100px] -translate-y-1/2 rounded bg-[#FFDBDC]"
           ></div>
         </div>
-        <div class="relative z-10">1.16790</div>
+        <div class="relative z-10">30.00</div>
       </div>
     </div>
 
     <!-- 5단계 호가 -->
     <div class="flex h-6 w-full gap-1">
       <div class="flex h-6 w-full items-center justify-between text-xs">
-        <div class="relative z-10">1.00</div>
+        <div class="relative z-10">50.00</div>
         <div class="relative">
-          <div class="relative z-10 mr-[9px]">1.16790</div>
+          <div class="relative z-10 mr-[9px]">1.35078</div>
           <div
             class="absolute right-0 top-1/2 h-6 w-[150px] -translate-y-1/2 rounded bg-[#E0EDFF]"
           ></div>
@@ -101,12 +101,12 @@
       </div>
       <div class="flex w-full items-center justify-between text-xs">
         <div class="relative z-10">
-          <div class="relative z-10 ml-[9px]">1.00</div>
+          <div class="relative z-10 ml-[9px]">1.35100</div>
           <div
             class="absolute left-0 top-1/2 h-6 w-[150px] -translate-y-1/2 rounded bg-[#FFDBDC]"
           ></div>
         </div>
-        <div class="relative z-10">1.16790</div>
+        <div class="relative z-10">50.00</div>
       </div>
     </div>
   </div>

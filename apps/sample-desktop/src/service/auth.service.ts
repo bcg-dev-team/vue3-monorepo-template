111559e9dfa803d48c20856a9228839a930d4036
pcjin <pcjin@blockchainglobal.co.kr>
2025-10-10 10:24:11 +0900

    chore(apps/sample-desktop): 로그아웃 처리 임시 alert 처리

--- apps/sample-desktop/src/service/auth.service.ts
+++ apps/sample-desktop/src/service/auth.service.ts
@@ -4,5 +4,8 @@ import router from '@/router';
 
 export const Logout = () => {
   LocalStorageService.removeItem(LocalStorageKey.ACCESS_TOKEN);
+
+  //FIXME: 임시 alert 처리
+  alert('세션이 만료 되었습니다. \n 다시 로그인 해주세요.');
   router.push({ name: 'login' });
 };

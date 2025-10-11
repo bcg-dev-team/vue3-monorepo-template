5a4efe21e971a1ecde69c49b94ed8a14ee8ec4bd
ygpark <ygpark@blockchainglobal.co.kr>
2025-10-11 13:37:16 +0900

    chore(api): 빌드 오류 수정

--- packages/api/src/services/index.ts
+++ packages/api/src/services/index.ts
@@ -1,5 +1,4 @@
 // 기존 API 서비스 (수동 작성)
-export { ExampleService } from './example/example.service';
 export { UserService } from './user.service';
 export { TermsService } from './terms.service';
 export { TradeService } from './trade.service';
